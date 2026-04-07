0c4ac2011d3feb00c3bf217605ef6167e951fc85
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-04-07 17:30:20 +0545

    a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Card from './components/Card';
+import Navbar from './components/Navbar';
 import Product from './components/Product';
 
 function App() {
   return (
     <>
       {/* <Card /> */}
+      <Navbar />
       <Product />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  width: fit-content;
+
+  margin: 2rem auto;
+  background: hsl(0 0% 0% / 0.8);
+  padding: 8px;
+  border-radius: 8px;
+  position: relative;
+
+  @supports (corner-shape: squircle) {
+    border-radius: 24px;
+    corner-shape: squircle;
+  }
+`;
+const NavList = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+`;
+const Nav = styled.li``;
+const NavItem = styled.a`
+  display: block;
+  padding: 1rem;
+  text-decoration: none;
+  color: white;
+`;
+
+function Navbar() {
+  return (
+    <Wrapper>
+      <NavList>
+        <Nav>
+          <NavItem href="#">Home</NavItem>
+        </Nav>
+        <Nav>
+          <NavItem href="#">About</NavItem>
+        </Nav>
+        <Nav>
+          <NavItem href="#">Blog</NavItem>
+        </Nav>
+        <Nav>
+          <NavItem href="#">Contact</NavItem>
+        </Nav>
+      </NavList>
+    </Wrapper>
+  );
+}
+
+export default Navbar;
